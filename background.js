cd79c72e383df5800690290ea60a489c741745f0
dasadboy <42483448+dasadboy@users.noreply.github.com>
2019-05-06 22:27:00 -0400

    Set initial newsPosition property values in local storage.

--- background.js
+++ background.js
@@ -1,5 +1,15 @@
 chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.local.set({newsFeeds: [], proxy: "", fav_ids: []},
+  chrome.storage.local.set({
+    newsFeeds: [],
+    proxy: "",
+    fav_ids: [],
+    newsPosition: {
+      top: null,
+      bottom: null,
+      left: null,
+      right: null
+    }
+  },
   () => {
     console.log(
       "Proxy set to '',\nnewsFeeds set to [],\nfav_ids set to []\n"
