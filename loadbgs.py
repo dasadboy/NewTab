10f67af217b5733e53326a73e9e980c5f4e04b93
dasadboy <42483448+dasadboy@users.noreply.github.com>
2019-05-09 19:45:21 -0400

    Reimplemented "loadbgs.py" for current project.

--- loadbgs.py
+++ loadbgs.py
@@ -7,19 +7,21 @@ def get_backgrounds():
     return bg_list
 
 def write_backgroundsjs():
-    backgrounds = get_backgrounds()
+    bg_list = os.listdir("backgrounds")
+    img_extentions = (".jpg", ".png", ".tiff", ".jpeg", ".tiff", ".gif")
+    bg_list = [b for b in bg_list if b.endswith(img_extentions)]
 
-    text = f"backgrounds = {backgrounds}\n"
+    lines = [f"backgrounds = [\n"]
+    
+    for bg in bg_list:
+        lines.append(f"\"{bg}\",\n") 
 
-    with open("backgrounds.js", "r") as bg_file:
-        lines = bg_file.readlines()
+    lines.append("]\n")
 
-    lines[0] = text
-
-    with open("backgrounds.js", "w") as bg_file:
+    with open("bgs.js", "w") as bg_file:
         bg_file.writelines(lines)
 
-    print(f"{len(backgrounds)} backgrounds have been loaded.")
+    print(f"{len(bg_list)} backgrounds have been loaded.")
 
 
 if __name__ == "__main__":
